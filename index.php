e0bc64749438f6c2cdd1b876769c4ec7a527be21
Adeniji Charles Ayodipupo <adenijiayocharles@gmail.com>
2020-03-17 01:37:11 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Coming Soon </title>
+    <link href="https://fonts.googleapis.com/css2?family=Sen:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/app.css" />
+    <link rel="stylesheet" href="assets/css/reset.css" />
+</head>
+<body>
+    <main>
+        <aside>
+            <div class="heading">
+                <h3> this website is under construction </h3>
+            </div>        
+        </aside>
+        <aside></aside>
+    </main>
+</body>
+</html>
